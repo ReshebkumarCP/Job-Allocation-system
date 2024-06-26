eda2a3e8d47dd942ddcee36e852cc327d30d230a
Resheb kumar CP <reshebkumarcp@Reshebs-MacBook-Air.local>
2024-06-26 22:30:37 +0530

    first commit

---
+++ 1234ambulance/lib/components/sevierity_btn.dart
@@ -0,0 +1,126 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:project/components/stop_btn.dart';
+
+
+class condition extends StatelessWidget {
+  const condition({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+        appBar: AppBar(
+       
+        ),
+        body: Center(
+        
+          
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              
+             const Padding(padding:EdgeInsets.all(16.0),
+             child: Align(
+              alignment: Alignment.centerLeft,
+              child: Text("click the situation:-",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 25,
+
+              ),
+              textAlign: TextAlign.left,
+              ),
+             ), 
+             ),
+             const SizedBox(height: 30,),
+              ElevatedButton(
+                onPressed: () {
+                  _insertClickEvent("red");
+                  
+                  Navigator.push(context, MaterialPageRoute(
+          builder: (builder)=>const StopBtn()
+          
+        ));
+                  
+                },
+                 style: ElevatedButton.styleFrom(
+          primary:Colors.red ,
+          minimumSize: const Size(200, 60),
+
+        ),
+                child: const Text('High',
+                style: TextStyle(color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 20.0
+                ),
+                
+                
+                ),
+              ),
+              const SizedBox(height: 16), 
+              ElevatedButton(
+                onPressed: () {
+                  _insertClickEvent("yellow");
+                  Navigator.push(context, MaterialPageRoute(
+          builder: (builder)=>const StopBtn()
+          
+        ));
+
+                },
+                 style: ElevatedButton.styleFrom(
+          primary:Colors.yellow ,
+          minimumSize: Size(200, 60),
+
+        ),
+                child: const Text('Moderate',
+                 style: TextStyle(color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 20.0
+                ),
+                ),
+              ),
+              const SizedBox(height: 16), 
+              ElevatedButton(
+                onPressed: () {
+                  _insertClickEvent("green");
+                  Navigator.push(context, MaterialPageRoute(
+          builder: (builder)=>const StopBtn()));
+                },
+                 style: ElevatedButton.styleFrom(
+          primary:Colors.green,
+          minimumSize: Size(200, 60),
+
+        ),
+                child: const Text('basic',
+                 style: TextStyle(color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 20.0
+                ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+  }
+}
+Future<void> 
+_insertClickEvent(String color)async{
+  CollectionReference sevierity = FirebaseFirestore.instance.collection("clicks");
+  //get current timestamp
+  final Timestamp= DateTime.now().toLocal().toString();
+
+  
+   //get the user name
+  String?_emailController=FirebaseAuth.instance.currentUser?.email;
+
+  //Insert data in firestore
+  await sevierity.add({
+    "timestamp": Timestamp,
+     "color": color,
+     "email":_emailController,
+
+  });
+}
---
+++ index.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+
+  <!-- Font Awesome CSS -->
+  <link rel="stylesheet" href="css/all.min.css">
+
+  <!-- Google Font -->
+  <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+
+  <!-- Custom CSS -->
+  <link rel="stylesheet" href="css/custom.css">
+
+  <title>CONNECT</title>
+</head>
+
+<body>
+  <!-- Start Navigation -->
+  <nav class="navbar navbar-expand-sm navbar-dark bg-danger pl-5 fixed-top">
+    <a href="index.php" class="navbar-brand">CONNECT</a>
+    <span class="navbar-text">Customer's Happiness is our Aim</span>
+    <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#myMenu">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="myMenu">
+      <ul class="navbar-nav pl-5 custom-nav">
+        <li class="nav-item"><a href="index.php" class="nav-link">Home</a></li>
+        <li class="nav-item"><a href="#Services" class="nav-link">Services</a></li>
+        <li class="nav-item"><a href="#registration" class="nav-link">Registration</a></li>
+        <li class="nav-item"><a href="Requester/RequesterLogin.php" class="nav-link">Login</a></li>
+        <li class="nav-item"><a href="#Contact" class="nav-link">Contact</a></li>
+      </ul>
+    </div>
+  </nav> <!-- End Navigation -->
+
+  <!-- Start Header Jumbotron-->
+  <header class="jumbotron back-image" style="background-image: url(images/Banner1.jpeg);">
+    <div class="myclass mainHeading">
+      <h1 class="text-uppercase text-danger font-weight-bold">Welcome to CONNECT</h1>
+      <p class="font-italic">Customer's Happiness is our Aim</p>
+      <a href="Requester/RequesterLogin.php" class="btn btn-success mr-4">Login</a>
+      <a href="#registration" class="btn btn-danger mr-4">Sign Up</a>
+    </div>
+  </header> <!-- End Header Jumbotron -->
+
+  <div class="container">
+    <!--Introduction Section-->
+    <div class="jumbotron">
+      <h3 class="text-center">CONNECT Services</h3>
+      <p>
+        CONNECT Services is India’s leading chain of multi-brand Electronics and Electrical service
+        workshops offering
+        wide array of services. We focus on enhancing your uses experience by offering world-class
+        Electronic
+        Appliances maintenance services. Our sole mission is “To provide Electronic Appliances care
+        services to
+        keep the devices fit and healthy and customers happy and smiling”.
+
+        With well-equipped Electronic Appliances service centres and fully trained mechanics, we
+        provide quality
+        services with excellent packages that are designed to offer you great savings.
+
+        Our state-of-art workshops are conveniently located in many cities across the country. Now you
+        can book
+        your service online by doing Registration.
+      </p>
+
+    </div>
+  </div>
+  <!--Introduction Section End-->
+  <!-- Start Services -->
+  <div class="container text-center border-bottom" id="Services">
+    <h2>Our Services</h2>
+    <div class="row mt-4">
+      <div class="col-sm-4 mb-4">
+        <a href="#"><i class="fas fa-tv fa-8x text-success"></i></a>
+        <h4 class="mt-4">Electronic Appliances</h4>
+      </div>
+      <div class="col-sm-4 mb-4">
+        <a href="#"><i class="fas fa-sliders-h fa-8x text-primary"></i></a>
+        <h4 class="mt-4">Preventive Maintenance</h4>
+      </div>
+      <div class="col-sm-4 mb-4">
+        <a href="#"><i class="fas fa-cogs fa-8x text-info"></i></a>
+        <h4 class="mt-4">Fault Repair</h4>
+      </div>
+    </div>
+  </div> <!-- End Services -->
+
+  <!-- Start Registration  -->
+  <?php include('userRegistration.php') ?>
+  <!-- End Registration  -->
+
+
+
+  <!--Start Contact Us-->
+  <div class="container" id="Contact">
+    <!--Start Contact Us Container-->
+    <h2 class="text-center mb-4">Contact US</h2> <!-- Contact Us Heading -->
+    <div class="row">
+
+      <!--Start Contact Us Row-->
+      <?php include('contactform.php'); ?>
+      <!-- End Contact Us 1st Column -->
+
+      <div class="col-md-4 text-center">
+        <!-- Start Contact Us 2nd Column-->
+        <strong>Headquarter:</strong> <br>
+        CONNECT Pvt Ltd, <br>
+        Sec IV, JEPPIYAR NAGAR <br>
+        chennai - 600051 <br>
+        Phone: +917358036463 <br>
+        <a href="#" target="_blank">www.CONNECT.com</a> <br>
+
+        <br><br>
+        <strong>chennai Branch:</strong> <br>
+        CONNECT Pvt Ltd, <br>
+        Ashok Nagar, TAMIL NADU <br>
+        CHENNAI - 600002 <br>
+        Phone: +7358036463 <br>
+        <a href="#" target="_blank">www.CONNECT.com</a> <br>
+      </div> <!-- End Contact Us 2nd Column-->
+    </div> <!-- End Contact Us Row-->
+  </div> <!-- End Contact Us Container-->
+  <!-- End Contact Us -->
+
+  <!-- Start Footer-->
+  <footer class="container-fluid bg-dark text-white mt-5" style="border-top: 3px solid #DC3545;">
+    <div class="container">
+      <!-- Start Footer Container -->
+      <div class="row py-3">
+        <!-- Start Footer Row -->
+        <div class="col-md-6">
+          <!-- Start Footer 1st Column -->
+          <span class="pr-2">Follow Us: </span>
+          <a href="#" target="_blank" class="pr-2 fi-color"><i class="fab fa-facebook-f"></i></a>
+          <a href="#" target="_blank" class="pr-2 fi-color"><i class="fab fa-twitter"></i></a>
+          <a href="#" target="_blank" class="pr-2 fi-color"><i class="fab fa-youtube"></i></a>
+          <a href="#" target="_blank" class="pr-2 fi-color"><i class="fab fa-google-plus-g"></i></a>
+          <a href="#" target="_blank" class="pr-2 fi-color"><i class="fas fa-rss"></i></a>
+        </div> <!-- End Footer 1st Column -->
+
+        <div class="col-md-6 text-right">
+          <!-- Start Footer 2nd Column -->
+          <small> Designed by TEAM SV &copy; 2023.
+          </small>
+          <small class="ml-2"><a href="Admin/login.php">Admin Login</a></small>
+        </div> <!-- End Footer 2nd Column -->
+      </div> <!-- End Footer Row -->
+    </div> <!-- End Footer Container -->
+  </footer> <!-- End Footer -->
+
+  <!-- Boostrap JavaScript -->
+  <script src="js/jquery.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/all.min.js"></script>
+</body>
+
+</html>
